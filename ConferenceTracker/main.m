0b424c1c79ed2851ed77db5092460c4ff10b5874
Burke Holland <burkeholland@gmail.com>
2012-02-14 09:10:39 -0600

    Initial Commit

---
+++ ConferenceTracker/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ConferenceTracker
+//
+//  Created by Burke Holland on 2/14/12.
+//  Copyright Telerik Inc. 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
